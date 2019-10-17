6037c12943541d6246c0bf3cc818a52a44ea927d
moddamatt <echomatt95@gmail.com>
2019-10-17 17:16:44 -0500

    Day 4 MVP complete

--- Class_Examples/simple.py
+++ Class_Examples/simple.py
@@ -6,6 +6,10 @@ PRINT_NUM      = 3
 SAVE           = 4  # SAVE VALUE INTO REGISTER
 PRINT_REGISTER = 5
 ADD            = 6
+PUSH           = 7
+POP            = 8
+CALL           = 9
+RET            = 10
 
 
 memory = [
@@ -29,6 +33,8 @@ running = True
 
 # Create 8 registers
 register = [0] * 8
+
+SP = 7
 
 while running:
     # Do stuff
@@ -63,6 +69,35 @@ while running:
         reg_b = memory[pc+2]   # Get the 2nd register index from 2nd arg
         register[reg_a] += register[reg_b] # Add registers, store in reg_a
         pc += 3
+
+    elif command == PUSH:
+        reg = memory[pc + 1]
+        val = register[reg]
+        # Decrement the SP.
+        register[SP] -= 1
+        # Copy the value in the given register to the address pointed to by SP.
+        memory[register[SP]] = val
+        pc += 2
+
+    elif command == POP:
+        reg = memory[pc + 1]
+        val = memory[register[SP]]
+        # Copy the value from the address pointed to by SP to the given register.
+        register[reg] = val
+        # Increment SP.
+        register[SP] += 1
+        pc += 2
+
+    elif command == CALL:
+        register[SP] -= 1
+        memory[register[SP]] = pc + 2
+
+        reg = memory[pc + 1]
+        pc = register[reg]
+
+    elif command == RET:
+        pc = memory[register[SP]]
+        register[SP] += 1
 
     else:
         print(f"Unknown instruction: {command}")
--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,23 +1,27 @@
 """CPU functionality."""
 
 import sys
+import time
 
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0b0] * 255    # 256 bytes of Memory
+        self.ram = [0b0] * 256    # 256 bytes of Memory
         self.reg = [0b0] * 8      # 8 general-purpose registers
 
         # Interal registers
         self.pc = 0
+        self.sp = 7
 
         # _opcode_
         self._opcode_ = { 
             0b10000010: 'LDI',
             0b01000111: 'PRN',
             0b00000001: 'HLT',
+            0b01000101: 'PSH',
+            0b01000110: 'POP',
 
             # PC mutators
             0b01010000: 'CALL',
@@ -34,34 +38,66 @@ class CPU:
             0b01100110: 'DEC',
         }
 
+        # self._dispatch_op_ = {}
+
+        # # ALU functions
+        # self._dispatch_op_['MUL'] = self.alu('MUL', )
+
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        try:
+            address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    # Process comments:
+                    # Ignore anything after a # symbol
+                    comment_split = line.split("#")
+                    # Convert any numbers from binary strings to integers
+                    num = comment_split[0].strip()
+                    try:
+                        x = int(num, 2)
+                    except ValueError:
+                        continue
+                    # print in binary and decimal
+                    self.ram[address] = x
+                    address += 1
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "DIV":
+            """
+            Divide the value in the first register by the value in the second,
+            storing the result in registerA.
+
+            If the value in the second register is 0, the system should print an
+            error message and halt.
+            """
+            pass
+        elif op == "MOD":
+            """
+            Divide the value in the first register by the value in the second,
+            storing the _remainder_ of the result in registerA.
+
+            If the value in the second register is 0, the system should print an
+            error message and halt.
+            """
+            pass
+        elif op == "INC":
+            self.reg[reg_a] += 1
+        elif op == "DEC":
+            self.reg[reg_a] -= 1
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -93,7 +129,9 @@ class CPU:
         while running:
             command = self.ram[self.pc]
 
-            # print(command)
+            print(self._opcode_[command])
+
+            self.trace()
 
             if self._opcode_[command] == 'LDI':
                 address = self.ram[self.pc + 1]
@@ -108,10 +146,56 @@ class CPU:
 
             elif self._opcode_[command] == 'HLT':
                 running = False
+            
+            elif self._opcode_[command] == 'ADD':
+                x = self.ram[self.pc + 1]
+                y = self.ram[self.pc + 2]
+                self.alu('ADD', x, y)
+                self.pc += 3
+
+            elif self._opcode_[command] == 'MUL':
+                x = self.ram[self.pc + 1]
+                y = self.ram[self.pc + 2]
+                self.alu('MUL', x, y)
+                self.pc += 3
+
+            elif self._opcode_[command] == 'PSH':
+                reg = self.ram[self.pc + 1]
+                val = self.reg[reg]
+                # Decrement the SP.
+                self.reg[self.sp] -= 1
+                # Copy the value in the given register to the address pointed to by SP.
+                self.ram[self.reg[self.sp]] = val
+                self.pc += 2
+
+            elif self._opcode_[command] == 'POP':
+                reg = self.ram[self.pc + 1]
+                val = self.ram[self.reg[self.sp]]
+                # Copy the value from the address pointed to by SP to the given register.
+                self.reg[reg] = val
+                # Increment self.sp.
+                self.reg[self.sp] += 1
+                self.pc += 2
+
+            elif self._opcode_[command] == 'CALL':
+                self.reg[self.sp] -= 1
+                self.ram[self.reg[self.sp]] = self.pc + 2
+
+                reg = self.ram[self.pc + 1]
+                self.pc = self.reg[reg]
+
+            elif self._opcode_[command] == 'RET':
+                self.pc = self.ram[self.reg[self.sp]]
+                self.reg[self.sp] += 1
+
+            elif self._opcode_[command] == 'JMP':
+                address = self.ram[self.pc + 1]
+                self.pc = address
 
             else:
                 print(f"Unknown instruction: {command}")
                 sys.exit(1)
+
 
         pass
 
